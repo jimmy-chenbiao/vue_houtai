01efd692098a613fdd6db0339db6ad33790ce030
chen <123@qwe.com>
2019-12-06 11:01:15 +0800

    完成订单功能的开发

---
+++ vue_shop/src/components/order/Order.vue
@@ -0,0 +1,154 @@
+<template>
+  <div><!--         面包屑导航区域-->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>订单管理</el-breadcrumb-item>
+      <el-breadcrumb-item>订单列表</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <!--    卡片区域-->
+    <el-card>
+      <el-row>
+        <el-col :span="8">
+          <el-input placeholder="请输入内容" v-model="input3" >
+            <el-button slot="append" icon="el-icon-search"></el-button>
+          </el-input>
+        </el-col>
+      </el-row>
+
+<!--      订单列表数据-->
+      <el-table :data="orderList" border stripe>
+        <el-table-column type="index"></el-table-column>
+        <el-table-column label="订单编号" prop="order_number">
+        </el-table-column>
+        <el-table-column label="订单价格" prop="order_price">
+        </el-table-column>
+        <el-table-column label="是否付款" prop="order_pay">
+          <template slot-scope="scope">
+            <el-tag type="success" v-if="scope.row.order_pay === '1'">已付款</el-tag>
+            <el-tag type="danger" v-else>未付款</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column label="是否发货" prop="is_send">
+        </el-table-column>
+        <el-table-column label="下单时间" prop="create_time">
+          <template slot-scope="scope">
+            {{scope.row.create_time | dateFormat}}
+          </template>
+        </el-table-column>
+        <el-table-column label="操作" >
+          <template slot-scope="scope">
+            <el-button type="primary" icon="el-icon-edit" size="mini" @click="showBox"></el-button>
+            <el-button type="success" icon="el-icon-location" size="mini" @click="showProgress"></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+<!--      分页区域-->
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="queryInfo.pagenum"
+        :page-sizes="[5, 10, 20, 30]"
+        :page-size="queryInfo.pagesize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total">
+      </el-pagination>
+    </el-card>
+
+<!--    修改对话框-->
+    <el-dialog title="修改地址" :visible.sync="addressVisible" width="50%" @close="dialogClose">
+      <el-form :model="addressForm" :rules="addressRules" ref="addressRef" label-width="100px" >
+        <el-form-item label="省市区/县" prop="address1">
+          <el-cascader :options="citydata" v-model="addressForm.address1"></el-cascader>
+        </el-form-item>
+        <el-form-item label="详细地址" prop="address2">
+          <el-input v-model="addressForm.address2"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+    <el-button @click="addressVisible = false">取 消</el-button>
+    <el-button type="primary" @click="addressVisible = false">确 定</el-button>
+  </span>
+    </el-dialog>
+
+<!--展示物流进度的对话框-->
+    <el-dialog title="提示" :visible.sync="progessVisible" width="50%">
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import citydata from './citydata.js'
+
+export default {
+  data() {
+    return {
+      queryInfo: {
+        query: '',
+        pagenum: 1,
+        pagesize: 10
+      },
+      orderList: [],
+      total: 0,
+      addressVisible: false,
+      addressForm: {
+        address1: [],
+        address2: ''
+
+      },
+      progessVisible: false,
+      progessInfo: [],
+      addressRules: {
+        address1: [
+          { required: true, message: '请选择省市区/县', trigger: 'blur' }
+        ],
+        address2: [
+          { required: true, message: '请输入修改的地址', trigger: 'blur' }
+        ]
+      },
+      citydata: citydata
+    }
+  },
+  created() {
+    this.getOrderList()
+  },
+  methods: {
+    async getOrderList() {
+      const { data: res } = await this.$http.get('orders', {
+        params: this.queryInfo
+      })
+      if (res.meta.status !== 200) return this.$message.error('获取数据失败')
+      // console.log(res)
+      this.orderList = res.data.goods
+      this.total = res.data.total
+    },
+    handleSizeChange(newSize) {
+      this.queryInfo.pagesize = newSize
+      this.getOrderList()
+    },
+    handleCurrentChange(newChange) {
+      this.queryInfo.pagenum = newChange
+      this.getOrderList()
+    },
+    // 展示修改地址的对话框
+    showBox() {
+      this.addressVisible = true
+    },
+    dialogClose() {
+      this.$refs.addressRef.resetFields()
+    },
+    async showProgress() {
+      const { data: res } = await this.$http.get('/kuaidi/804909574412544580')
+      if (res.meta.status !== 200) return this.$message.error('获取物流进度失败')
+      this.progessInfo = res.data
+      this.progessVisible = true
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.el-cascader {
+  width: 100%;
+}
+</style>
